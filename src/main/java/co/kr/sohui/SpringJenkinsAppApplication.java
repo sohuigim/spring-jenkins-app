48043359d6530e4c4a15154c154b98ea8aea7a3c
sohuigim <aisurudake@gmail.com>
2024-10-11 11:40:10 +0900

    first

---
+++ src/main/java/co/kr/sohui/SpringJenkinsAppApplication.java
@@ -0,0 +1,13 @@
+package co.kr.sohui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/kr/sohui/SpringJenkinsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.sohui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
